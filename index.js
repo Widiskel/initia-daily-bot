e4b9f0afea0eb9b8f4255f06162c50617d605447
widiskel <waputra16@gmail.com>
2024-05-27 13:05:36 +0700

    mod (initia) modify the async await

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 import * as initia from "./src/initia.js";
 import * as routine from "./src/initia_routine.js";
 import { account } from "./src/account.js";
-const TOTALSEND = 10;
-const TOTALSWAP = 10;
+const TOTALSEND = 5;
+const TOTALSWAP = 5;
 
 async function doQuest(walletAddress, privateKey) {
   return new Promise(async (resolve, reject) => {
     try {
-      await initia.initiation(walletAddress, privateKey);
-      console.log(`Account ${walletAddress} Information`);
-      const initBalance = await initia.queryBalance();
-      console.log();
-      console.log("Doing daily routine for Account " + walletAddress);
-      if (initBalance.amount / 1000000 < 20) {
-        reject(
-          `Balance < 20 Initia for account ${walletAddress}, Please request initia token from faucet `
-        );
-      } else {
-        console.log("1. Send 1 Init to Other for Account" + walletAddress);
-        console.log();
-        for (let send = 0; send < TOTALSEND; send++) {
-          await routine.sendOneInitToOther();
-        }
-        console.log("2. Swap 1 INIT to USDC for Account" + +walletAddress);
-        console.log();
-        for (let swap = 0; swap < TOTALSWAP; swap++) {
-          await routine.swap();
-        }
+      await initia
+        .initiation(walletAddress, privateKey)
+        .then(async () => {
+          await initia
+            .queryBalance()
+            .then(async (initBalance) => {
+              console.log(`Account ${walletAddress} Information`);
+              console.log();
+              console.log("Doing daily routine for Account " + walletAddress);
+              if (initBalance.amount / 1000000 < 20) {
+                reject(
+                  `Balance < 20 Initia for account ${walletAddress}, Please request initia token from faucet `
+                );
+              } else {
+                console.log(
+                  "1. Send 1 Init to Other for Account" + walletAddress
+                );
+                console.log();
+                for (let send = 0; send < TOTALSEND; send++) {
+                  await routine.sendOneInitToOther();
+                }
+                console.log(
+                  "2. Swap 1 INIT to USDC for Account" + +walletAddress
+                );
+                console.log();
+                for (let swap = 0; swap < TOTALSWAP; swap++) {
+                  await routine.swap();
+                }
 
-        // console.log("3. Claim EXP");
-        // await routine.claimExp();
-      }
-      resolve();
+                // console.log("3. Claim EXP");
+                // await routine.claimExp();
+                resolve();
+              }
+            })
+            .then((error) => reject(error));
+        })
+        .then((error) => reject(error));
     } catch (error) {
       reject(error);
     }
