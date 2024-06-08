30e0da14b51bbfffef57c0c7dec1a75b533a0785
widiskel <waputra16@gmail.com>
2024-06-08 20:01:17 +0700

    feat (initia) add vote function with latest proposal available

--- src/repository/initia_repo.js
+++ src/repository/initia_repo.js
@@ -81,28 +81,32 @@ async function getReferalPoint() {
 }
 
 async function getLatestProposal() {
-  const response = await fetch(
-    "https://api.initiation-1.initia.xyz/indexer/gov/v1/proposals?status=all",
-    {
-      headers: {
-        accept: "application/json, text/plain, */*",
-      },
-      referrer: "https://app.testnet.initia.xyz/",
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: null,
-      method: "GET",
-      mode: "cors",
-      credentials: "omit",
-    }
-  );
-
-  if (response.ok) {
-    const data = await response.json();
-    return data.proposals[0];
-  } else {
-    throw new Error(
-      "Failed to fetch stage info data. Status: " + response.statusText
+  try {
+    const response = await fetch(
+      "https://api.initiation-1.initia.xyz/indexer/gov/v1/proposals?status=all",
+      {
+        headers: {
+          accept: "application/json, text/plain, */*",
+        },
+        referrer: "https://app.testnet.initia.xyz/",
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: null,
+        method: "GET",
+        mode: "cors",
+        credentials: "omit",
+      }
     );
+
+    if (response.ok) {
+      const data = await response.json();
+      return data.proposals[0];
+    } else {
+      throw new Error(
+        "Failed to fetch proposals data. Status: " + response.statusText
+      );
+    }
+  } catch (error) {
+    throw error;
   }
 }
 
